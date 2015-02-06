41acfab9a0ac45acf3bb2fc520ae968e6181893f
Branislav Blaskovic <bblaskov@redhat.com>
2015-02-07 00:35:59 +0100

    First try

--- bstock.go
+++ bstock.go
@@ -1,8 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
-func main() {
-    fmt.Printf("bstock.\n")
+// Download downloads URL and returns it
+func GetPrice(url string) float64 {
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	text := doc.Find("#ctl00_BCPP_Celkem_dvCelkem td.num").First().Text()
+	text = strings.Replace(text, ",", ".", -1)
+
+	price, err := strconv.ParseFloat(text, 32)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		os.Exit(1)
+	}
+
+	return price
+
 }
 
+func main() {
+	fmt.Printf("bstock by Branislav Blaskovic\n")
+
+	// BAASTOCK
+	price := GetPrice("http://www.bcpp.cz/Cenne-Papiry/Detail.aspx?isin=GB00BF5SDZ96")
+	fmt.Printf("BAASTOCK %.2f CZK\n", price)
+}
